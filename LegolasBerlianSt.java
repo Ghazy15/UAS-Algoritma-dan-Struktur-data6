dc069d322732acd54ebd58a7ef8a306b39e16562
Muhammad Naufal Alifauzi <147986997+mnaufal411@users.noreply.github.com>
2024-06-08 22:10:09 +0700

    Update LegolasBerlianSt.java

--- LegolasBerlianSt.java
+++ LegolasBerlianSt.java
@@ -32,3 +32,20 @@ public class LegolasBerlian {
     	scanner.close();
 	}
 }
+
+private static boolean jalurHamiltonian(int kotaAwal, int aliansiAwal, int nKota, Map<Integer, List<Integer>> mapAliansi, Map<String, List<String>> graph) {
+	Stack<Node> stack = new Stack<>();
+	stack.push(new Node(kotaAwal, aliansiAwal, new HashSet<>(Collections.singleton(kotaAwal)), new ArrayList<>(Collections.singleton(kotaAwal))));
+
+	while (!stack.isEmpty()) {
+    	Node node = stack.pop();
+    	int kotaSt = node.kotaNow;
+    	int aliansiSt = node.aliansiIni;
+    	Set<Integer> kotaThen = node.kotaThen;
+    	List<Integer> link = node.link;
+
+    	if (kotaThen.size() == nKota) {
+        	return true;
+    	}
+
+}
